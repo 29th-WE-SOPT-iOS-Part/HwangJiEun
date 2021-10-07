9fe5e8513f554e2bd093ae018999643963529c91
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-07 23:30:54 +0900

    ➕[ADD] : 프로젝트 폴더링 및 Sign Process 파일 생성

---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UIColor+Additions.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Additions.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/10/07.
+//
+
+import Foundation
---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Screens/Sign/Controllers/SignUpVC.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpVC.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/10/07.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

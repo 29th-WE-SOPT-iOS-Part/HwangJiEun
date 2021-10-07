5203adf4d9097c3e961fe801952bff1e341bf05d
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-08 02:56:40 +0900

    ➕[ADD] : UITextField+Extension 추가
    addLeftPadding 함수 추가

--- 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UITextfield+Extension.swift
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UITextfield+Extension.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 extension UITextField {
+    /// addLeftPadding - TextField에서 왼쪽 여백을 width 너비만큼 주는 함수입니다.
     func addLeftPadding() {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: self.frame.height))
         self.leftView = paddingView

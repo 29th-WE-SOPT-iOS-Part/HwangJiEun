ea9e1a43283a6af6393682aeb14a700d88afeebb
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-22 04:15:31 +0900

    ➕[ADD] : 에셋 및 파일 추가(UIButton+, SignTopReusableView)

---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UIButton+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+Extension.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/10/22.
+//
+
+import Foundation
---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/VIew/SignTopReusableView.swift
@@ -0,0 +1,20 @@
+//
+//  SignTopReusableView.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/10/22.
+//
+
+import UIKit
+
+class SignTopReusableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

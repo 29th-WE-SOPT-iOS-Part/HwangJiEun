fe8c7c045c9be32e2468a83a8e8cf8094f74351f
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-22 04:18:25 +0900

    ✅[CHORE] : UIButton Extesion 추가 - state에 따라 UIButton backgroundColor 바꾸기 위한 함수 추가

--- 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UIButton+Extension.swift
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UIButton+Extension.swift
@@ -5,4 +5,23 @@
 //  Created by 황지은 on 2021/10/22.
 //
 
-import Foundation
+import UIKit
+extension UIButton {
+    /// UIButton의 State에 따라 backgroundColor를 변경하는 함수입니다.
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        let minimumSize: CGSize = CGSize(width: 1.0, height: 1.0)
+        
+        UIGraphicsBeginImageContext(minimumSize)
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(origin: .zero, size: minimumSize))
+        }
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.clipsToBounds = true
+        self.setBackgroundImage(colorImage, for: state)
+    }
+}

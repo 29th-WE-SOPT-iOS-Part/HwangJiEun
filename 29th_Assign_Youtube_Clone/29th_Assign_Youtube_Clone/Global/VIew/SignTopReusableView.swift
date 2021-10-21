4af6c076cf56165390777efe0cd74da00f7e99ae
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-22 04:20:24 +0900

    ➕[ADD] : Sign 상단 재사용뷰 생성 - Google로고, HeadLabel, DescLabel 묶어 ReusableView로 생성

--- 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/VIew/SignTopReusableView.swift
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/VIew/SignTopReusableView.swift
@@ -7,14 +7,59 @@
 
 import UIKit
 
+enum SignState {
+    case signIn, signUp, welcome
+}
+
 class SignTopReusableView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    let xibName = "SignTopReusableView"
+    
+    @IBOutlet var signHeadTextLabel: UILabel!
+    @IBOutlet var signDescTextLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
-
+    
+    func setTextLabelFontStyle(_ division: SignState, _ headText: String, _ descText: String) {
+        //signHeadTextLabel
+        signHeadTextLabel.font = UIFont(name: "SFProDisplay-Semibold", size: 24)
+        signHeadTextLabel.textColor = .googleDarkGray
+        signHeadTextLabel.text = headText
+        signHeadTextLabel.numberOfLines = 0
+        signHeadTextLabel.textAlignment = .center
+        signHeadTextLabel.sizeToFit()
+        
+        //signDescTextLabel
+        signDescTextLabel.font = UIFont(name: "SFProDisplay-Regular", size: 14)
+        signDescTextLabel.textColor = .googleDarkGray
+        signDescTextLabel.text = descText
+        signDescTextLabel.numberOfLines = 0
+        signDescTextLabel.textAlignment = .center
+        signDescTextLabel.sizeToFit()
+        
+        switch division {
+        case .signIn:
+            signDescTextLabel.isHidden = false
+        case .signUp:
+            signDescTextLabel.isHidden = true
+        case .welcome:
+            signDescTextLabel.isHidden = true
+            signHeadTextLabel.font = UIFont(name: "SFProDisplay-Semibold", size: 26)
+            signHeadTextLabel.text = headText + "님 \n환영합니다!"
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
 }

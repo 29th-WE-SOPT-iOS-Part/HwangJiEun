b31510f492bc9f8c618ce744918f5343634d9b1b
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-22 04:46:31 +0900

    ✅[CHORE] : TabBarSB 추가 및 폴더링 변경

---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Screens/TabBar/Controller/YoutubeTBC.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeTBC.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/10/22.
+//
+
+import UIKit
+
+class YoutubeTBC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}

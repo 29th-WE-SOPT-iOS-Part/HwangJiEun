5de516bebd89522d6257470318b7d34fe2d75d09
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-22 04:21:36 +0900

    ✅[CHORE] : SignConfirmVC - 상단 재사용뷰 적용, Autolayout 적용, 코드 수정

--- 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Screens/Sign/Controllers/SignConfirmVC.swift
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Screens/Sign/Controllers/SignConfirmVC.swift
@@ -11,34 +11,44 @@ class SignConfirmVC: UIViewController {
     
     static let identifier = "SignConfirmVC"
     var userName: String?
-
-    @IBOutlet var userWelcomeLabel: UILabel!
+    
+    @IBOutlet var signConfirmTopView: SignTopReusableView! {
+        didSet {
+            if let user = userName {
+                signConfirmTopView.setTextLabelFontStyle(.welcome, user, "")
+            }
+        }
+    }
     @IBOutlet var confirmBtn: UIButton!
+    @IBOutlet var otherAccountBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUserWelcomeLabelText()
-        setConfirmBtnAttributes(fontSize: 15, cornerRadius: 10)
+        setConfirmBtnAttributes(fontSize: 14, cornerRadius: 10)
+        setOtherAccountBtnAttributes(fontSize: 14)
     }
     
-    /// userName 전달받아 구성하는 함수
-    func setUserWelcomeLabelText() {
-        if let user = userName {
-            userWelcomeLabel.text = "\(user)님 \n환영합니다!"
-            userWelcomeLabel.sizeToFit()
-        }
-    }
-
     /// confirmBtn Attributes 설정
     func setConfirmBtnAttributes(fontSize: CGFloat, cornerRadius: CGFloat) {
         confirmBtn.setTitle("확인", for: .normal)
-        confirmBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: fontSize)
+        confirmBtn.titleLabel?.font = UIFont(name: "SFProDisplay-Medium", size: fontSize)
         confirmBtn.titleLabel?.tintColor = .white
         confirmBtn.backgroundColor = .googleBlue
         confirmBtn.layer.cornerRadius = cornerRadius
     }
     
-    @IBAction func dismissBtnDidTap(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+    func setOtherAccountBtnAttributes(fontSize: CGFloat) {
+        //signUpBtn
+        otherAccountBtn.setTitle("다른 계정으로 로그인하기", for: .normal)
+        otherAccountBtn.titleLabel?.font = UIFont(name: "SFProDisplay-Medium", size: fontSize)
+        otherAccountBtn.titleLabel?.textColor = .googleBlue
+    }
+    
+    @IBAction func confirmBtnDidTap(_ sender: UIButton) {
+       // 탭바VC로 이동 + rootVC 설정
+    }
+    
+    @IBAction func otherAccountBtnDidTap(_ sender: UIButton) {
+        // 로그인 화면으로 dismiss
     }
 }

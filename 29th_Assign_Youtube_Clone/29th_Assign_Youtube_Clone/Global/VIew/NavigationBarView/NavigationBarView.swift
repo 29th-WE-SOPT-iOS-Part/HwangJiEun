fce7ce60be712fef0d3fafa35d7a711359b583f8
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-29 15:58:44 +0900

    ✨[FEAT] : NavigationBarView Xib 생성
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/VIew/NavigationBarView/NavigationBarView.swift
@@ -0,0 +1,90 @@
+//
+//  NavigationBarView.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/10/29.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class NavigationBarView: UIView {
+    
+    //Mark: - Properties
+    private var mainIconImage = UIImageView().then {
+        $0.image = UIImage(named: "premiumLogo")
+    }
+    private var windowSharingBtn = UIButton().then {
+        $0.setBackgroundImage(UIImage(named: "windowSharingIcon"), for: .normal)
+    }
+    private var notificationBtn = UIButton() .then {
+        $0.setBackgroundImage(UIImage(named: "notificationIcon"), for: .normal)
+    }
+    private var searchBtn = UIButton() .then {
+        $0.setBackgroundImage(UIImage(named: "SearchIcon"), for: .normal)
+    }
+    private var profileBtn = UIButton().then {
+        $0.setBackgroundImage(UIImage(named: "wesoptProfile"), for: .normal)
+    }
+    
+    //Mark :- Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+        makeLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let view = Bundle.main.loadNibNamed(identifiers.NavigationBarView, owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
+
+//MARK: - Layout
+extension NavigationBarView {
+    func makeLayout() {
+        self.addSubviews([mainIconImage, windowSharingBtn, notificationBtn, searchBtn, profileBtn])
+        
+        mainIconImage.snp.makeConstraints {
+            $0.top.equalTo(self).offset(12)
+            $0.leading.equalTo(self).offset(16)
+            $0.height.equalTo(20)
+            $0.width.equalTo(96)
+        }
+        
+        profileBtn.snp.makeConstraints {
+            $0.trailing.equalTo(self).offset(-16)
+            $0.height.equalTo(24)
+            $0.width.equalTo(profileBtn.snp.height).multipliedBy(1.0 / 1.0)
+            $0.centerY.equalTo(mainIconImage)
+        }
+        
+        searchBtn.snp.makeConstraints {
+            $0.trailing.equalTo(profileBtn.snp.leading).offset(-17)
+            $0.height.equalTo(32)
+            $0.width.equalTo(searchBtn.snp.height).multipliedBy(1.0 / 1.0)
+            $0.centerY.equalTo(profileBtn)
+        }
+        
+        notificationBtn.snp.makeConstraints {
+            $0.trailing.equalTo(searchBtn.snp.leading).offset(-11)
+            $0.height.equalTo(32)
+            $0.width.equalTo(notificationBtn.snp.height).multipliedBy(1.0 / 1.0)
+            $0.centerY.equalTo(searchBtn)
+        }
+        
+        windowSharingBtn.snp.makeConstraints {
+            $0.trailing.equalTo(notificationBtn.snp.leading).offset(-12)
+            $0.height.equalTo(32)
+            $0.width.equalTo(windowSharingBtn.snp.height).multipliedBy(1.0 / 1.0)
+            $0.centerY.equalTo(notificationBtn)
+        }
+    }
+}

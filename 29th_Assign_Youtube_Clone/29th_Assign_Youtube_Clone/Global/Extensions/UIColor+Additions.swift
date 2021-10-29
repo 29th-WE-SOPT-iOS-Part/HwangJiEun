ae8dfbd845bb826d02e55e490d65c4d4f47fb431
hwangJi-dev <melon2228@swu.ac.kr>
2021-10-29 15:58:03 +0900

    ✅[CHORE] : UIColor 추가 - labelGray, tagBorderGray, tagBackgroundGray

--- 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UIColor+Additions.swift
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Extensions/UIColor+Additions.swift
@@ -19,4 +19,16 @@ extension UIColor {
     @nonobjc class var googleLightGray: UIColor {
         return UIColor(red: 234.0 / 255.0, green: 234.0 / 255.0, blue: 234.0 / 255.0, alpha: 1.0)
     }
+    
+    @nonobjc class var labelGray: UIColor {
+        return UIColor(red: 96.0 / 255.0, green: 96.0 / 255.0, blue: 96.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var tagBorderGray: UIColor {
+        return UIColor(red: 218.0 / 255.0, green: 218.0 / 255.0, blue: 218.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var tagBackgroundGray: UIColor {
+        return UIColor(red: 242.0 / 255.0, green: 242.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
+    }
 }

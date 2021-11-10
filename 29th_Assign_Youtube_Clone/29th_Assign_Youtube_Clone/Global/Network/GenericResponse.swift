eec766f547ba3c1f735c61328e7dc7af1ff79f5e
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-11 03:19:27 +0900

    ➕[ADD] : Network 통신을 위한 기본 세팅(APIConstants, GenericResponse, NetworkResult)

---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Global/Network/GenericResponse.swift
@@ -0,0 +1,29 @@
+//
+//  GenericResponse.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/11/11.
+//
+
+import Foundation
+struct GenericResponse<T: Codable>: Codable {
+    var status: Int
+    var success: Bool
+    var message: String
+    var data: T?
+    
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case success = "success"
+        case message = "message"
+        case data = "data"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(Int.self, forKey: .status)) ?? -1
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode(T.self, forKey: .data)) ?? nil
+    }
+}

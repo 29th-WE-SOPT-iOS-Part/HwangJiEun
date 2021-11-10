ba469ad7dc69ede726097adf8ce730bee7aee6fb
hwangJi-dev <melon2228@swu.ac.kr>
2021-11-11 03:23:42 +0900

    ✅[CHORE] : SignService 생성 - 로그인, 회원가입, 회원조회

---
+++ 29th_Assign_Youtube_Clone/29th_Assign_Youtube_Clone/Screens/Sign/Service/SignService.swift
@@ -0,0 +1,140 @@
+//
+//  SignService.swift
+//  29th_Assign_Youtube_Clone
+//
+//  Created by 황지은 on 2021/11/11.
+//
+
+import Foundation
+import Alamofire
+
+struct SignService {
+    static let shared = SignService()
+    
+    //MARK: - signUp(회원가입)
+    func signUp(email: String,
+                name: String,
+                password: String,
+                completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        
+        let url = APIConstants.signUpURL
+        
+        let header: HTTPHeaders = [
+            "Content-Type" : "application/json"
+        ]
+        
+        let body: Parameters = [
+            "email": email,
+            "name": name,
+            "password": password
+        ]
+        print(body)
+        let dataRequest = AF.request(url,
+                                     method: .post,
+                                     parameters: body,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        dataRequest.responseData { (response) in
+            switch response.result {
+            case .success :
+                guard let statusCode = response.response?.statusCode else {
+                    return
+                }
+                guard let data = response.value else {
+                    return
+                }
+                completion(judgeData(status: statusCode, data: data))
+                
+            case .failure(let err) :
+                print(err)
+            }
+        }
+    }
+    
+    //MARK: - login(로그인)
+    func login(email: String,
+               password: String,
+               completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        
+        let url = APIConstants.loginURL
+        
+        let header: HTTPHeaders = [
+            "Content-Type" : "application/json"
+        ]
+        
+        let body: Parameters = [
+            "email": email,
+            "password": password
+        ]
+        print(body)
+        let dataRequest = AF.request(url,
+                                     method: .post,
+                                     parameters: body,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        dataRequest.responseData { (response) in
+            switch response.result {
+            case .success :
+                guard let statusCode = response.response?.statusCode else {
+                    return
+                }
+                guard let data = response.value else {
+                    return
+                }
+                completion(judgeData(status: statusCode, data: data))
+                
+            case .failure(let err) :
+                print(err)
+            }
+        }
+    }
+    
+    //MARK: - getUser(유저 조회)
+    func getUser(userId: Int,
+                 completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        
+        let url = APIConstants.getUserURL + "\(userId)"
+        
+        let header: HTTPHeaders = [
+            "Content-Type" : "application/json"
+        ]
+        
+        let dataRequest = AF.request(url,
+                                     method: .get,
+                                     parameters: nil,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        dataRequest.responseData { (response) in
+            switch response.result {
+            case .success :
+                guard let statusCode = response.response?.statusCode else {
+                    return
+                }
+                guard let data = response.value else {
+                    return
+                }
+                completion(judgeData(status: statusCode, data: data))
+                
+            case .failure(let err) :
+                print(err)
+            }
+        }
+    }
+    
+    private func judgeData(status: Int, data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<SignResultData>.self, from: data) else {
+            return .pathErr }
+        
+        switch status {
+        case 200:
+            return .success(decodedData.data ?? "None-Data")
+        case 400..<500:
+            return .requestErr(decodedData.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
